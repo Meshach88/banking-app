b66e38e207e419494bf05fce882bd31eaaa93aad
Meshach Esuabom <meshachogochukwu@gmail.com>
2023-10-27 12:06:56 +0100

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Billings from "./Billings";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeals from "./CardDeals";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedBackCards from "./FeedBackCards";
+import Footer from "./Footer";
+import GettingStarted from "./GettingStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+
+
+export {
+    Button,
+    Billings,
+    Business,
+    CardDeals,
+    Clients,
+    CTA,
+    FeedBackCards,
+    Footer,
+    GettingStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials
+};
